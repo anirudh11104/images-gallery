220152e52694a5caf7fa422d4d4544ef63974b4e
Anirudh R <anirudhreddy11104@gmail.com>
2025-09-09 00:48:22 +0530

    image author info

--- frontend/src/ImageCard.js
+++ frontend/src/ImageCard.js
@@ -1,20 +1,36 @@
-import React from "react";
-import { Card, Button } from "react-bootstrap";
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-const ImageCard = ({image, deleteImage, saveImage}) => {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={image.urls.small} />
-            <Card.Body>
-                <Card.Title>{image.title?.toUpperCase()}</Card.Title>
-                <Card.Text>
-                    {image.description || image.alt_description}
-                </Card.Text>
-                <Button variant="primary" onClick= {() => deleteImage(image.id)}>Delete</Button>{' '}
-                {!image.saved && (<Button variant="secondary" onClick={() => saveImage(image.id)}>Save</Button>)}
-            </Card.Body>
-        </Card>
-    )
-}
+const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image.urls.small} />
+      <Card.Body>
+        <Card.Title>{image.title?.toUpperCase()}</Card.Title>
+        <Card.Text>{image.description || image.alt_description}</Card.Text>
+        <Button variant="primary" onClick={() => deleteImage(image.id)}>
+          Delete
+        </Button>{' '}
+        {!image.saved && (
+          <Button variant="secondary" onClick={() => saveImage(image.id)}>
+            Save
+          </Button>
+        )}
+      </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL ? (
+          <Card.Link href={authorPortfolioURL} target="_blank" rel="noopener noreferrer">
+            {authorName}
+          </Card.Link>
+        ) : (
+          authorName
+        )}
+      </Card.Footer>
+    </Card>
+  );
+};
 
 export default ImageCard;
